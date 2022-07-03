209adda75f230cc3f42f5f38f8df6755c8c9ce64
Jean-Lionel <nijeanlionel@gmail.com>
2022-07-03 18:51:09 +0200

    add some thing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Database\Seeders\DeviseSeeder;
 
@@ -15,6 +17,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = [
+            'name' => "JEAN LEO",
+            'email' => "nijeanlionel@gmail.com",
+            'email_verified_at' => now(),
+            'password' => bcrypt("12345678"), // password
+            'remember_token' => Str::random(10),
+        ];
+
+        User::create($user);
 
         $this->call(DeviseSeeder::class);
     }
